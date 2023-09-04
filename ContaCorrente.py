efc2c4d25d29384d59b5b2b49d19e868ca2281d4
jaimecabrito01 <103322510+jaimecabrito01@users.noreply.github.com>
2023-09-04 10:01:39 -0300

    Update ContaCorrente.py

--- ContaCorrente.py
+++ ContaCorrente.py
@@ -1,11 +1,34 @@
 import Conta
 class ContaCorrente(Conta):
-    def __init__(self,limiteAtual,limiteDaConta):
+    def __init__(self,numero,saldo,limiteAtual,limiteDaConta):
+        super().__init__(numero,saldo)
+        if self.saldo == 0:
+            self.limiteDaConta = self.limiteDaConta
         self.limiteAtual = limiteAtual
-        self.limiteDaConta = limiteDaConta
+     
+    
 
 
-    def ConsultarLimiteDaConta():
-
-    def ConsultarSaldoTotal():
+    def ConsultarLimiteDaConta(self):
+       return print(f"Limite da conta: {self.limiteDaConta}")
         
+    def ConsultarSaldoTotal(self):
+       return print(f"Saldo total: {self.saldo + self.limiteAtual}")
+   
+    def sacar(self,valor):
+          if valor <= self.saldo + self.limiteAtual:
+            if valor <= self.saldo:
+                self.saldo -= valor
+            else:
+                valor_excedente = valor - self.saldo
+                self.saldo = 0
+                self.limiteAtual -= valor_excedente
+            print(f"Saque de {valor} realizado.")
+            
+            print(f"Limite atual: {self.limiteAtual}")
+            
+    def depositar(self,valor):
+        if self.limiteAtual < self.limiteDaConta:
+            while self.limiteAtual == self.limiteDaConta:
+                self.limiteAtual += self.limiteDaConta
+            self.saldo += valor
